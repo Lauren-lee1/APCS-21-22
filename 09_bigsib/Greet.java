d3503521f80b544ba3fa3eaa89c96d99b3cb99bc
Lauren Lee <llee30@stuy.edu>
2021-10-01 20:05:36 -0400

    add hw09

---
+++ 09_bigsib/Greet.java
@@ -0,0 +1,27 @@
+/*
+Cool Computers: Lauren Lee, Boe, Gloria Lee, Kevin Li
+APCS
+HW09: What are BigSibs good for?
+2021-10-1
+*/
+
+/*
+DISCOVERIES:
+- to invoke a method from another class in a  different file, use the syntax Class.method()
+- BigSib did not need a main method to be incorporated into the main method of Greet
+
+UNRESOLVED QUESTIONS:
+- What is the benefit to invoking a class from a different file as a helper method?
+*/
+
+
+public class Greet {
+	public static void main (String [] args) {
+		BigSib.greet("Lauren");
+		BigSib.greet("Gloria");
+		BigSib.greet("Nora");
+	}
+
+
+}
+
