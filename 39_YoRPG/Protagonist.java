104f4f7b7344a48758bdf856f11ed497d172ee6b
Lauren Lee <llee30@stuy.edu>
2021-11-22 13:48:07 -0500

    edit rpg

---
+++ 39_YoRPG/Protagonist.java
@@ -0,0 +1,20 @@
+public class Protagonist {
+
+  private String toBeCalled;
+
+  public Protagonist() {
+      toBeCalled = "Harry";
+  }
+
+  public Protagonist(String name) {
+    toBeCalled = name;
+  }
+
+  public String getName() {
+    return toBeCalled;
+  }
+
+  public int attack(Monster name) {
+    
+  }
+}
