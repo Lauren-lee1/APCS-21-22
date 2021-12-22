f17f3c8d6b6446221a89443292c2e0b6be2cad0c
Lauren Lee <llee30@stuy.edu>
2021-12-22 14:04:29 -0500

    cw

---
+++ 49_rat/Driver.java
@@ -0,0 +1,13 @@
+public class Driver{
+
+  public static void main(String[] args){
+    Comparable n = new Rational();
+    n.Rational();
+    Rational bob = new Rational();
+    if () {
+      System.out.println(bob.makeRat(n));
+    } else{
+      System.out.println("error");
+    }
+  }
+}
