f1cb6fdbc28f66090e4ca8c3718e57462af2af21
Lauren Lee <llee30@stuy.edu>
2022-02-15 12:30:15 -0500

    finish two group

--- cb/recursion-2/codingBat.java
+++ cb/recursion-2/codingBat.java
@@ -33,12 +33,66 @@ start: 3 target: 6 (14) 8
 start 2 target: 10 (16) 4
 start 3 target 2 (14)8
 
-
-
 */
+/*
+Given an array of ints, is it possible to choose a group of some of the ints,
+beginning at the start index, such that the group sums to the given target?
+However, with the additional constraint that all 6's must be chosen. (No loops needed.)
+*/
+  public static boolean groupSum6(int start, int[] nums, int target) {
+    if (start >= nums.length){
+      if (target == 0)
+        return true;
+    } else if (nums[start] == 6){
+      return groupSum6(start+1, nums, target - nums[start]);
+    }
+      else if (groupSum6(start+1, nums, target - nums[start]) == true) {
+      return true;
+    } else if (groupSum6(start+1, nums, target) == true){
+      return true;
+    }
+     return false;
+  }
 
+/*
+Given an array of ints, is it possible to choose a group of some of the ints,
+such that the group sums to the given target with this additional constraint:
+If a value in the array is chosen to be in the group, the value immediately
+following it in the array must not be chosen. (No loops needed.)
+*/
+  public static boolean groupNoAdj(int start, int[] nums, int target){
+    if (start >= nums.length){
+      return (target == 0);
+    } else if (groupNoAdj(start + 2, nums, target - nums[start])) {
+      return true;
+    } else if (groupNoAdj(start+1, nums, target)){
+      return true;
+    }
+      return false;
+  }
 
-
+  public static boolean groupSum5(int start, int[] nums, int target){
+    if (start >= nums.length){
+      if (target == 0)
+        return true;
+    }else if (nums[start] % 5 == 0 && start+1 < nums.length && nums[start+1] == 1) {
+      return groupSum5(start+2, nums, target - nums[start]);
+    } else if (nums[start] % 5 == 0){
+      return groupSum5(start+1, nums, target - nums[start]);
+    }
+      else if (groupSum5(start+1, nums, target - nums[start]) == true) {
+      return true;
+    } else if (groupSum5(start+1, nums, target) == true){
+      return true;
+    }
+     return false;
+  }
+/*
+Given an array of ints, is it possible to divide the ints into two groups,
+so that the sums of the two groups are the same. Every int must be in one group
+or the other. Write a recursive helper method that takes whatever arguments you
+like, and make the initial call to your recursive helper from splitArray(). (No loops needed.)
+*/
   public static boolean splitArray(int[] nums){
     return add(nums, 0, 0, 0);
   }
@@ -73,5 +127,18 @@ start 3 target 2 (14)8
     System.out.println(splitArray(c) + "...should be false");
     System.out.println(splitArray(d) + "...should be true");
 
+    int [] e = new int [] {5,6,2};
+    System.out.println(groupSum6(0, e, 8) + "...should be true");
+    System.out.println(groupSum6(0, e, 9) + "...should be false");
+    System.out.println(groupSum6(0, e, 7) + "...should be false");
+
+    int [] f = new int [] {2, 5, 10, 4};
+    System.out.println(groupNoAdj(0, f, 12) + "...should be true");
+    System.out.println(groupNoAdj(0, f, 14) + "...should be false");
+    System.out.println(groupNoAdj(0, f, 7) + "...should be false");
+
+    System.out.println(groupSum5(0, f, 19) + "...should be true");
+    System.out.println(groupSum5(0, f, 17) + "...should be true");
+    System.out.println(groupSum5(0, f, 12) + "...should be false");
   }
 }
