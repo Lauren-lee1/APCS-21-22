f302badfa05976c361738dfc61bb57e186958547
Lauren Lee <llee30@stuy.edu>
2022-02-15 22:56:42 -0500

    cb

--- cb/recursion-2/codingBat.java
+++ cb/recursion-2/codingBat.java
@@ -71,6 +71,12 @@ following it in the array must not be chosen. (No loops needed.)
       return false;
   }
 
+/*
+Given an array of ints, is it possible to choose a group of some of the ints,
+such that the group sums to the given target with these additional constraints:
+ all multiples of 5 in the array must be included in the group. If the value immediately
+  following a multiple of 5 is 1, it must not be chosen. (No loops needed.)
+*/
   public static boolean groupSum5(int start, int[] nums, int target){
     if (start >= nums.length){
       if (target == 0)
@@ -79,14 +85,34 @@ following it in the array must not be chosen. (No loops needed.)
       return groupSum5(start+2, nums, target - nums[start]);
     } else if (nums[start] % 5 == 0){
       return groupSum5(start+1, nums, target - nums[start]);
-    }
-      else if (groupSum5(start+1, nums, target - nums[start]) == true) {
+    } else if (groupSum5(start+1, nums, target - nums[start]) == true) {
       return true;
     } else if (groupSum5(start+1, nums, target) == true){
       return true;
     }
      return false;
   }
+
+/*
+Given an array of ints, is it possible to choose a group of some of the ints,
+such that the group sums to the given target, with this additional constraint:
+if there are numbers in the array that are adjacent and the identical value,
+they must either all be chosen, or none of them chosen. For example, with the
+array {1, 2, 2, 2, 5, 2}, either all three 2's in the middle must be chosen or not,
+all as a group. (one loop can be used to find the extent of the identical values).
+*/
+ public static boolean groupSumClump(int start, int[] nums, int target){
+   if (start > = nums.length){
+     return target == 0;
+   } else if (nums[start] == nums[start + 1]){
+     return groupSumClump(start+1, nums, target - nums[start])
+   } else if (groupSumClump(start+1, nums, target - nums[start])) {
+     return true;
+   } else if (groupSumClump(start+1, nums, target)){
+     return true;
+   }
+ }
+
 /*
 Given an array of ints, is it possible to divide the ints into two groups,
 so that the sums of the two groups are the same. Every int must be in one group
@@ -140,5 +166,11 @@ like, and make the initial call to your recursive helper from splitArray(). (No 
     System.out.println(groupSum5(0, f, 19) + "...should be true");
     System.out.println(groupSum5(0, f, 17) + "...should be true");
     System.out.println(groupSum5(0, f, 12) + "...should be false");
+
+    int [] g = new int [] {1, 2, 4, 8, 1};
+    int [] g = new int [] {1, 4, 4, 8};
+    System.out.println(groupSumClump(0, a, 10) + "...should be true");
+    System.out.println(groupSumClump(0, f, 14) + "...should be true");
+    System.out.println(groupSumClump(0, f, 14) + "...should be false");
   }
 }
