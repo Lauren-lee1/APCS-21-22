9f1eb4437b53e63fee23505dbbd9ba0b10df7c4c
Lauren Lee <llee30@stuy.edu>
2022-04-05 13:43:41 -0400

    lab100 started; activity2

---
+++ cobo/data/DataStudentCode/Cereal.java
@@ -0,0 +1,13 @@
+public class Cereal{
+  private char type;
+  private int calories;
+  private int[8] nutrition;
+  private int shelf;
+  private double weight;
+  private double cups;
+  private double rating;
+
+  public Cereal(){
+
+  }
+}
