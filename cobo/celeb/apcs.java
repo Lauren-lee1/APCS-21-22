852a686d1c4abd200ae5132f17a5f6577b30a567
Lauren Lee <llee30@stuy.edu>
2022-04-26 23:03:59 -0400

    celeb day 2 pt 3

---
+++ cobo/celeb/apcs.java
@@ -0,0 +1,80 @@
+import java.lang.*;
+public class apcs{
+
+  //false -- junior
+  //true -- senior
+  private boolean _grade;
+  private int _period;
+  private String _teacher;
+
+  public apcs(boolean grade, int period, String teacher){
+    _grade = grade;
+    _period = period;
+    _teacher = teacher;
+  }
+
+  public boolean getGrade(){
+    return _grade;
+  }
+
+  public int getPeriod(){
+    return _period;
+  }
+
+  public String getTeacher(){
+    return _teacher;
+  }
+
+  public void setGrade(boolean grade){
+    _grade = grade;
+  }
+
+  public boolean switchPeriod(int period){
+    int rand = (int)(Math.random()*2);
+    boolean study = false;
+    if (rand == 0){
+      study = true;
+    }
+    if(takeTest(study)<65 || isLate()){
+      _period = (int)(Math.random()*8 + 3);
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  public boolean switchTeacher(String teacher){
+    int rand = (int)(Math.random()*2);
+    boolean study = false;
+    if (rand == 0){
+      study = true;
+    }
+    if(takeTest(study)<65){
+      _teacher=teacher;
+      return true;
+    } else{
+      return false;
+    }
+  }
+
+  public int takeTest(boolean study){
+    if(study){
+      return (int) (Math.random() * 30)+65;
+    } else{
+      return (int) (Math.random()*65);
+    }
+  }
+
+  public boolean hasSenioritis(){
+    return _grade;
+  }
+
+  public boolean isLate(){
+    if (_period<3){
+      return true;
+    } else{
+      return false;
+    }
+  }
+
+}
---
+++ cobo/celeb/mykolyk.java
@@ -0,0 +1,30 @@
+import java.lang.*;
+public class mykolyk extends apcs{
+
+  private double _hrsSpent;
+  private int _daysSinceGrassTouching;
+  private boolean _holidayMaths;
+  private String _SOTW;
+
+  public mykolyk(boolean grade, int period, double hr, int days, boolean holiday, String song){
+    super(grade, period, "Mykolyk");
+    _hrsSpent = hr;
+    _daysSinceGrassTouching = days;
+    _holidayMaths = holiday;
+    _SOTW = song;
+  }
+
+  @Override
+  public int takeTest(boolean study){
+    if (_holidayMaths == true && _hrsSpent <= 0.5 && _daysSinceGrassTouching == 0 && _SOTW.equals("Ride or Die") && study){
+      return 100;
+    } else if(_holidayMaths == true && _hrsSpent <= 0.5 && study){
+      return (int) (Math.random()*34 + 65);
+    } else {
+      return (int) (Math.random()*65);
+    }
+  }
+
+
+
+}
